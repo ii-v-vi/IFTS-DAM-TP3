e6423d096296558385a95aa156a0a74b20d43418
Lucas Luccaroni <lucas_luccaroni@hotmail.com>
2026-05-08 13:16:15 -0300

    Activity Â' evoNoSocio' casi completa a- Falta lógica de btnVolver,, +óguca de footer + estilado final
    CobrarActividad1Activty  creada

--- app/src/main/java/com/example/dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni/NuevoNoSocioActivity.kt
+++ app/src/main/java/com/example/dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni/NuevoNoSocioActivity.kt
@@ -1,20 +1,63 @@
 package com.example.dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.Button
+import android.widget.Toast
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class NuevoNoSocioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_nuevo_no_socio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnContinuar = findViewById<Button>(R.id.btnContinuar)
+        val cardZumba = findViewById<LinearLayout>(R.id.llCardZumba)
+        val cardPilates = findViewById<LinearLayout>(R.id.llCardPilates)
+        val cardElongacion = findViewById<LinearLayout>(R.id.llCardElongacion)
+        val cardKarate = findViewById<LinearLayout>(R.id.llCardKarate)
+        val cardSpinning = findViewById<LinearLayout>(R.id.llCardSpinning)
+        val cardHit = findViewById<LinearLayout>(R.id.llCardHit)
+        var cardSeleccionada : LinearLayout? = null
+        var todasLascCards = listOf<LinearLayout>(cardZumba, cardPilates, cardElongacion, cardKarate, cardSpinning, cardHit)
+
+
+        // ---------- SELECCIONAR LAS CARDS ----------
+        // Funcion para pintar la card seleccionada y despintar la anterior (si la hay)
+        fun seleccionarCard(card: LinearLayout){
+
+            // Despintamos la card seleccionada previamente
+            cardSeleccionada?.setBackgroundColor(Color.parseColor("#DF4A47"))
+
+            // Pintamos la nueva card elegida
+            card.setBackgroundColor(Color.parseColor("#E3F7D4"))
+
+            // Remplazamos la instancia de la cardSeleccionada vieja por la nueva
+            cardSeleccionada = card
         }
+
+        // Aplicamos un forEach para recorrer el array de cards y aplicar la funcion de pintar en cada una
+        todasLascCards.forEach { card ->
+            card.setOnClickListener {
+                seleccionarCard(card)
+            }
+        }
+        // ---------- SELECCIONAR LAS CARDS ----------
+
+
+        // ---------- BOTON CONTINUAR ----------
+        btnContinuar.setOnClickListener {
+            if(cardSeleccionada == null){
+                Toast.makeText(this, "Debe seleccionar una actividad", Toast.LENGTH_LONG).show()
+            }
+            else{
+                var intentarCobrarActividad1 = Intent(this, CobrarActividad1Activity::class.java)
+                startActivity(intentarCobrarActividad1)
+            }
+        }
+        // ---------- BOTON CONTINUAR ----------
     }
 }
