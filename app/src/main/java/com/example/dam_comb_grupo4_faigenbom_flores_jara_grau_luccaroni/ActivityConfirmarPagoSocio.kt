09a1a4612b2c639adf9dae755dd85cfe4dc4f390
Lucas Luccaroni <lucas_luccaroni@hotmail.com>
2026-05-10 11:52:21 -0300

    debug de activities luego de poder correrlas | logica de botones pendiente

--- app/src/main/java/com/example/dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni/ActivityConfirmarPagoSocio.kt
+++ app/src/main/java/com/example/dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni/ActivityConfirmarPagoSocio.kt
@@ -1,20 +1,12 @@
 package com.example.dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class ActivityConfirmarPagoSocio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_confirmar_pago_socio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
