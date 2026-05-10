80fb2368eb58254ca641ecc578622b4b8ca5e47c
Ivan Ariel Faigenbom <ivanfaigenbom@gmail.com>
2026-05-10 09:49:40 -0300

    Activity conf pago

---
+++ app/src/main/java/com/example/dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni/ActivityConfirmarPago.kt
@@ -0,0 +1,20 @@
+package com.example.dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityConfirmarPago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_confirmar_pago)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
