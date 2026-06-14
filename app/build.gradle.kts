165f7d8655dbe61035119be0d2052a2dfdf1fce0
ii-v-vi <ivanfaigenbom@gmail.com>
2026-06-14 18:58:14 -0300

    Sume tantas cosas que ya no sé que poner en el comit xd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.dam_comb_grupo4_faigenbom_flores_jara_grau_luccaroni"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
